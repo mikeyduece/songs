f05097e8aeadee21a765c1601ddc8b0770bacdce
Mike Heft <mikeheft@gmail.com>
2020-03-19 06:48:12 +0000

    Add title and duration conditional rendering for SongDetail

--- src/components/SongDetail.jsx
+++ src/components/SongDetail.jsx
@@ -1,9 +1,20 @@
 import React       from 'react'
 import { connect } from 'react-redux'
 
-const SongDetail = () => {
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Select a Song</div>
+  }
+
   return (
-    <div>SongDetail</div>
+    <div>
+      <h3>Details for:</h3>
+      <p>
+        Title: { song.title }
+        <br />
+        Duration: { song.duration }
+      </p>
+    </div>
   )
 }
 
