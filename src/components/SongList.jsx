73f49197673885e9e4fe4aa1ebd6ff923b5bc77a
Mike Heft <mikeheft@gmail.com>
2020-03-19 06:29:31 +0000

    Add selectSong action to SongList

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,6 +1,7 @@
 // Dependencies
 import React, { Component } from 'react'
 import { connect }          from 'react-redux'
+import { selectSong }       from '../actions'
 
 class SongList extends Component {
   renderList() {
@@ -8,16 +9,17 @@ class SongList extends Component {
       return (
         <div className='item' key={ song.title }>
           <div className='right floated content'>
-            <button className='ui button primary'>
+            <button
+              className='ui button primary'
+              onClick={() => this.props.selectSong(song)}
+            >
               Select
             </button>
-
           </div>
 
           <div className='content'>
             { song.title }
           </div>
-
         </div>
       )
     })
@@ -26,14 +28,18 @@ class SongList extends Component {
   render() {
     return (
       <div className='ui divided list'>
-        {this.renderList()}
+        { this.renderList() }
       </div>
     )
   }
 }
 
 const mapStateToProps = (state) => {
+  console.log(state)
   return { songs: state.songs }
 }
 
-export default connect(mapStateToProps)(SongList)
+export default connect(
+  mapStateToProps,
+  { selectSong }
+)(SongList)
